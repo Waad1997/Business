e109698ce5d1df5c724a6a92bbcc2d56ffbe95d2
Waad1997 <waadasiri@gmail.com>
2020-03-15 22:16:02 +0300

    1st commit

---
+++ app/src/main/java/com/example/business/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.business;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         * ################### activity_main.xml ####################
+         * DONE TODO 1: LinearLayout View Group
+         * TODO 2: RelativeLayout View Group
+         * DONE TODO 3: Business name
+         * DONE TODO 4: ImageView --> Business pic
+         * DONE TODO 5: Business Info
+         * DONE TODO 6: Add TextView --> sp
+         * DONE TODO 7: Other elements --> dp
+         * ###########################################################
+         */
+
+    }
+}
